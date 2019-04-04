3f641a967e5774e3e6ab7f95b98a83531ebc0a39
ALMLAMBO <aleksm007148@gmail.com>
2019-04-04 14:11:52 +0300

    Final version of the homework

--- 2019-03-23-football-statistics-hw/football-stats.sql
+++ 2019-03-23-football-statistics-hw/football-stats.sql
@@ -29,8 +29,6 @@ create table Players (
   foreign key (PositionCode) references Positions(PositionCode)
 );
 
-alter table Players modify column PositionCode varchar(2) after Num;
-
 -- 5. Създайте таблица "Tournaments" с турнири, в
 -- които участва отбора, със следната структура и данни:
 
@@ -64,84 +62,84 @@ create table MatchStats (
 );
 
 insert into StatTypes values
-                             ('G', 'Гол'),
-                             ('A', 'Асистенция'),
-                             ('R', 'Червен картон'),
-                             ('Y', 'Жълт картон'),
-                             ('OG', 'Автогол'),
-                             ('IN', 'Смяна влиза'),
-                             ('OUT', 'Смяна излиза');
+                      ('G', 'Гол'),
+                      ('A', 'Асистенция'),
+                      ('R', 'Червен картон'),
+                      ('Y', 'Жълт картон'),
+                      ('OG', 'Автогол'),
+                      ('IN', 'Смяна влиза'),
+                      ('OUT', 'Смяна излиза');
 
 -- select * from StatTypes;
 
 insert into Positions values
-                             ('GK', 'Вратар'),
-                             ('RB', 'Десен защитник'),
-                             ('LB', 'Ляв защитник'),
-                             ('CB', 'Централен защитник'),
-                             ('RM', 'Десен полузащитник'),
-                             ('LM', 'Ляв полузащитник'),
-                             ('CM', 'Полузащитник'),
-                             ('CF', 'Централен нападател');
+                      ('GK', 'Вратар'),
+                      ('RB', 'Десен защитник'),
+                      ('LB', 'Ляв защитник'),
+                      ('CB', 'Централен защитник'),
+                      ('RM', 'Десен полузащитник'),
+                      ('LM', 'Ляв полузащитник'),
+                      ('CM', 'Полузащитник'),
+                      ('CF', 'Централен нападател');
 
 -- select * from Positions;
 
 insert into Players values
-                           (null, 'Ivaylo Trifonov', 1, 'GK'),
-                           (null, 'Valko Trifonov', 2, 'RB'),
-                           (null, 'Ognyan Yanev', 3, 'CB'),
-                           (null, 'Zahari Dragomirov', 4, 'CB'),
-                           (null, 'Bozhidar Chilikov', 5, 'LB'),
-                           (null, 'Timotei Zahariev', 6, 'CM'),
-                           (null, 'Marin Valentinov', 7, 'CM'),
-                           (null, 'Mitre Cvetkov', 99, 'CF'),
-                           (null, 'Zlatko Genov', 9, 'CF'),
-                           (null, 'Matey Goranov', 10, 'RM'),
-                           (null, 'Sergei Zhivkov', 11, 'LM');
+                    (null, 'Ivaylo Trifonov', 1, 'GK'),
+                    (null, 'Valko Trifonov', 2, 'RB'),
+                    (null, 'Ognyan Yanev', 3, 'CB'),
+                    (null, 'Zahari Dragomirov', 4, 'CB'),
+                    (null, 'Bozhidar Chilikov', 5, 'LB'),
+                    (null, 'Timotei Zahariev', 6, 'CM'),
+                    (null, 'Marin Valentinov', 7, 'CM'),
+                    (null, 'Mitre Cvetkov', 99, 'CF'),
+                    (null, 'Zlatko Genov', 9, 'CF'),
+                    (null, 'Matey Goranov', 10, 'RM'),
+                    (null, 'Sergei Zhivkov', 11, 'LM');
 
 -- select * from Players;
 
 insert into Tournaments values
-                               (null, 'Шампионска лига'),
-                               (null, 'Първа лига'),
-                               (null, 'Купа на България'),
-                               (null, 'Суперкупа на България');
+                        (null, 'Шампионска лига'),
+                        (null, 'Първа лига'),
+                        (null, 'Купа на България'),
+                        (null, 'Суперкупа на България');
 
 -- select * from Tournaments;
 
 insert into Matches values
-                           (null, '2018-04-08', 2),
-                           (null, '2018-04-13', 2),
-                           (null, '2018-04-21', 2),
-                           (null, '2018-04-28', 2),
-                           (null, '2018-05-06', 2),
-                           (null, '2018-05-11', 2),
-                           (null, '2017-09-21', 3),
-                           (null, '2017-10-26', 3);
+                    (null, '2018-04-08', 2),
+                    (null, '2018-04-13', 2),
+                    (null, '2018-04-21', 2),
+                    (null, '2018-04-28', 2),
+                    (null, '2018-05-06', 2),
+                    (null, '2018-05-11', 2),
+                    (null, '2017-09-21', 3),
+                    (null, '2017-10-26', 3);
 
 -- select * from Matches;
 
 insert into MatchStats values
-                              (null, 8, 9, 14, 'G'),
-                              (null, 8, 8, 14, 'A'),
-                              (null, 8, 3, 43, 'Y'),
-                              (null, 7, 2, 28, 'Y'),
-                              (null, 7, 10, 45, 'Y'),
-                              (null, 7, 10, 65, 'R'),
-                              (null, 1, 10, 23, 'G'),
-                              (null, 1, 9, 23, 'A'),
-                              (null, 1, 9, 43, 'G'),
-                              (null, 2, 4, 33, 'OG'),
-                              (null, 2, 9, 68, 'G'),
-                              (null, 2, 1, 68, 'A'),
-                              (null, 3, 3, 35, 'G'),
-                              (null, 3, 4, 35, 'A'),
-                              (null, 3, 8, 55, 'G'),
-                              (null, 3, 11, 55, 'A'),
-                              (null, 4, 3, 9, 'G'),
-                              (null, 4, 8, 9, 'G'),
-                              (null, 4, 8, 56, 'OG'),
-                              (null, 5, 8, 67, 'G');
+                      (null, 8, 9, 14, 'G'),
+                      (null, 8, 8, 14, 'A'),
+                      (null, 8, 3, 43, 'Y'),
+                      (null, 7, 2, 28, 'Y'),
+                      (null, 7, 10, 45, 'Y'),
+                      (null, 7, 10, 65, 'R'),
+                      (null, 1, 10, 23, 'G'),
+                      (null, 1, 9, 23, 'A'),
+                      (null, 1, 9, 43, 'G'),
+                      (null, 2, 4, 33, 'OG'),
+                      (null, 2, 9, 68, 'G'),
+                      (null, 2, 1, 68, 'A'),
+                      (null, 3, 3, 35, 'G'),
+                      (null, 3, 4, 35, 'A'),
+                      (null, 3, 8, 55, 'G'),
+                      (null, 3, 11, 55, 'A'),
+                      (null, 4, 3, 9, 'G'),
+                      (null, 4, 8, 9, 'G'),
+                      (null, 4, 8, 56, 'OG'),
+                      (null, 5, 8, 67, 'G');
 
 -- select * from MatchStats;
 
@@ -163,7 +161,7 @@ select m.MatchDate, t.Name
           on m.TournamentId = t.Id
           having MatchDate
           between '2018-04-01'
-          and '2018-04-30';;
+          and '2018-04-30';
 
 -- 10. Направете заявка за статистиките на играч с
 -- номер на фланелката 99 със следните колони:
@@ -173,9 +171,16 @@ select m.MatchDate, t.Name
 -- Минута на събитието
 -- Четимият текст за събитието в мача
 
-
+select m.MatchDate, p.Name,
+       p.Num, ms.EventMinute,
+       t.Name from MatchStats ms
+        left join Matches m on ms.MatchId = m.Id
+        left join Players p on ms.PlayerId = p.Id
+        left join Tournaments t on m.TournamentId = t.Id
+        where p.Num = 99;
 
 -- 11. Направете заявка за общия брой автоголове на отбора
+
 select count(StatCode)
         from MatchStats
         where StatCode = 'OG';
@@ -185,6 +190,13 @@ select count(StatCode)
 -- Дата на мача
 -- Брой вкарани голове в този мач
 
+select m.MatchDate,
+       count(ms.StatCode)
+        from Matches m
+        left join MatchStats ms on m.Id = ms.MatchId
+        and ms.StatCode = 'G'
+        where m.MatchDate < '2018-05-01'
+        group by m.MatchDate;
 
 -- 13. Направете заявка за броя на головете според позицията
 -- на играчите със следните колони:
@@ -193,7 +205,14 @@ select count(StatCode)
 -- Забележка: включете всички позиции в резултата,
 -- дори да няма вкарани голове от играчи на тези позиции.
 
-
+select pos.Name,
+        count(ms.StatCode)
+        from Players p
+        left join MatchStats ms on p.Id = ms.PlayerId
+        right join Positions pos
+        on p.PositionCode = pos.PositionCode
+        and ms.StatCode = 'G'
+        group by pos.Name;
 
 -- 14. Направете заявка за общия брой на картоните (жълти и червени)
 -- за всеки играч, който има такива,
@@ -203,3 +222,13 @@ select count(StatCode)
 -- Позиция в отбора като четим текст
 -- Брой получени картони
 
+select p.Name, p.Num, pos.Name,
+       count(ms.StatCode)
+        from MatchStats ms
+        left join Players p on ms.PlayerId = p.Id
+        left join Positions pos
+        on p.PositionCode = pos.PositionCode
+        where ms.StatCode = 'Y'
+        or ms.StatCode = 'R'
+        group by p.Name
+        order by count(ms.StatCode) desc;
